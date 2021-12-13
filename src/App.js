8b98b7e1d98f20e8518306bbb36e0012c0195e52
Baran Öden <baranoden@gmail.com>
2021-12-13 15:12:31 +0300

    Project Finished

--- src/App.js
+++ src/App.js
@@ -2,29 +2,59 @@ import React, { useState, useEffect } from "react";
 import Movie from "./components/Movie";
 
 const FEATURED_API =
-"https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
 
 const SEARCH_API =
-"https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
-
-
-
+  "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 
 function App() {
- 
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
-    fetch(FEATURED_API)
-      .then((res) => res.json())
-      .then((data) => {
-        setMovies(data.results);
-      });
+    getMovies(FEATURED_API)
   }, []);
 
-  return <div>
-    {movies.length > 0 && movies.map((movie) => <Movie key={movie.id} data={movie}/>)}
-    </div>;
+
+  const getMovies = (API) => {
+    fetch(API)
+      .then((res) => res.json())
+      .then((data) => {
+        setMovies(data.results)
+      });
+  }
+  const handleOnSubmit = (e) => {
+    e.preventDefault()
+
+    if(searchTerm) {
+      getMovies(SEARCH_API + searchTerm)
+    
+    setSearchTerm("")}
+  };
+  const handleOnChange = (e) => {
+    setSearchTerm(e.target.value)
+  };
+
+  return (
+    <>
+      <header>
+        <form onSubmit={handleOnSubmit}>
+          <input
+            className="search"
+            type="text"
+            placeholder="Search a Movie"
+            value={searchTerm}
+            onChange={handleOnChange}
+          ></input>
+        </form>
+      </header>{" "}
+      <div className="movie-container">
+        {movies.length > 0 &&
+          movies.map((movie) => <Movie key={movie.id} {...movie} />)}
+      </div>
+      
+    </>
+  )
 }
 
 export default App;
--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,15 +1,39 @@
-import React from 'react'
+import React from "react";
 
-const IMG_API = "https://image.tmdb.org/t/p/w1280"
+const IMG_API = "https://image.tmdb.org/t/p/w1280";
+const setVoteClass = (vote) => {
+  if (vote >= 8) {
+    return "green";
+  } else if (vote >= 6) {
+    return "orange";
+  } else {
+    return "red";
+  }
+};
 
-
-function Movie({title, poster_path, overview, vote_average}) {
-    return (
-        <div className="movie">
-            This is a movie component
-            
-        </div>
-    )
+function Movie({ title, poster_path, overview, vote_average }) {
+  return (
+    <div className="movie">
+      <img
+        src={
+          poster_path
+            ? IMG_API + poster_path
+            : "https://cdn03.ciceksepeti.com/cicek/kc8523797-1/L/clapperboard-sinema-klaketi-dekoratif-hediyelik-sinema-klaket-kc8523797-1-e5ee57fc3b05450aa5767028d61d8fe6.jpg"
+        }
+        alt={title}
+      />
+      <div className="movie-info">
+        <h3>{title}</h3>
+        <span className={`tag ${setVoteClass(vote_average)}`}>
+          {vote_average}
+        </span>
+      </div>
+      <div className="movie-over">
+        <h2>Overview:</h2>
+        <p>{overview}</p>
+      </div>
+    </div>
+  );
 }
 
-export default Movie
+export default Movie;
